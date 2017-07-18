05e52a354003502d18224fc99512e86b45fa42cd
hkadivar <hkadivar@unc.edu>
2017-07-18 13:31:30 -0400

    converted rubaiyat

--- hkadivar-convert-docs.sh
+++ hkadivar-convert-docs.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 #Declare input argument as a variable
-Rubaiyat=rubaiyat.md
+INPUTFILE=rubaiyat
+OUTPUTNAME=$INPUTFILE
 
 # Convert markdown to HTML
-echo "`rubaiyat`"
+echo "converting $INPUTFILE to html"
+pandoc -o $OUTPUTNAME.html $INPUTFILE.md
 
 # Convert markdown to DOCX
+echo "converting $INPUTFILE to docx"
+pandoc -o $OUTPUTNAME.docx $INPUTFILE.md
 
 # Convert markdown to ODT
+echo "converting $INPUTFILE to odt"
+pandoc -o $OUTPUTNAME.odt $INPUTFILE.md
 
 # Convert markdown to PDF
+echo "converting $inputfile to pdf"
+pandoc -o $OUTPUTNAME.pdf --latex-engine=pdflatex $INPUTFILE.md
+
+echo "converting $INPUTFILE to txt"
+pandoc -o $OUTPUTNAME.txt $INPUTFILE.md
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
-read rubaiyat
+echo "Converted $OUTPUTNAME.md to HTML, DOCX, ODT, PDF and TXT"
